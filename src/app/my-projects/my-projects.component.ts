590e76df8004da3ce81908353b88e6f7bd9f57ee
Jan Steindorf <funstyle@hotmail.de>
2022-02-04 03:47:19 +0100

    added first responsive + fixed footer & projects

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-my-projects',
@@ -7,9 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
 
+
+  projectName = [
+    'Beispiel Website1',
+    'Beispiel Website2',
+    'Beispiel Website3',
+    'Beispiel Website4',
+    'Beispiel Website5',
+  ]
+
+  projectImg = [
+    '/assets/img/prototype.png',
+  ]
+
+  projectText = [
+    'This is a example text'
+  ]
+
+  projectLink = [
+    '#'
+  ]
+
+
+  All = true;
+  Angular = true;
+  JavaScript = true;
+  Unity = true;
+
+  //grid : any;
+
+  ngOnInit(): void {
+    //this.grid = document.querySelector("#grid");
+  }
+
+  over(int : number) {
+    console.log(`project-${int}`)
+    console.log(document.querySelector("project1"));
+  }
+
+  exit() {
+    console.log("Exit");
+  }
+
+
+  OnClick(name: string) {
+
+    console.log(name);
+
+    if (name == "All") {
+      this.All = true;
+      this.Angular = false;
+      this.JavaScript = false;
+      this.Unity = false;
+    } else if (name == "Angular") {
+      this.All = false;
+      this.Angular = true;
+      this.JavaScript = false;
+      this.Unity = false;
+    } else if (name == "JavaScript") {
+      this.All = false;
+      this.Angular = false;
+      this.JavaScript = true;
+      this.Unity = false;
+    } else if (name == "Unity") {
+      this.All = false;
+      this.Angular = false;
+      this.JavaScript = false;
+      this.Unity = true;
+    }
+  }
 }
--- src/app/skillrotation/skillrotation.component.ts
+++ src/app/skillrotation/skillrotation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skillrotation',
@@ -7,9 +7,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillrotationComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  /*
+      < div class = "main-container-object" >
+        <div class="main-container-object-head">
+          <img src="assets/img/skills/unityIcon.png" >
+            </div>
+            < div class="main-container-object-body" >
+              <p>Unity Engine < /p>
+                < /div>
+                < /div>
+  */
+
+  skillName = [
+    "Angular",
+    "Blender 3D",
+    "Canva",
+    "C#",
+    "CSS",
+    "GitHub",
+    "HTML",
+    "Illustrator",
+    "JavaScript",
+    "Adobe Photoshop",
+    "Slack",
+    "Unity Engine",
+    "Zoom",
+  ]
+
+  skillImage = [
+    "assets/img/skills/angularIcon.svg",
+    "assets/img/skills/blenderIcon.png",
+    "assets/img/skills/canvaIcon.png",
+    "assets/img/skills/cSharpIcon.png",
+    "assets/img/skills/css3IconIcon.png",
+    "assets/img/skills/githubIcon.png",
+    "assets/img/skills/html5Icon.png",
+    "assets/img/skills/illustratorIcon.png",
+    "assets/img/skills/javascriptIcon.png",
+    "assets/img/skills/photoshopIcon.png",
+    "assets/img/skills/slackIcon.png",
+    "assets/img/skills/unityIcon.png",
+    "assets/img/skills/zoomIcon.png",
+  ]
+
+  skillBool = [
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+    "false",
+  ]
+
+  doPlay = false;
+
+  group1Count = 0;
+  group2Count = 0;
+  group3Count = 0;
+  
+  constructor() {
+    
+  } 
+
+
+  ngOnInit() {
+    //let test = document.getElementById('group1');
+    //this.update();
   }
+
+  update() {
+
+    setInterval(() => {
+    }, 8000);
+
+    setInterval(() => {
+    }, 1000 / 30);
+  }
+
+
 
 }
