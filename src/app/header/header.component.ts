f7dfeb803e16eadc05053f268ff84fbd9fff6c98
Jan Steindorf <funstyle@hotmail.de>
2022-02-06 03:02:32 +0100

    responsive update + new mobile head

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MyProjectsComponent } from '../my-projects/my-projects.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  classActive = false;
+  routerLinkVariable = "/appRoutes";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  changeMenu() {
+    console.log("Test");
+
+    if (this.classActive == true) {
+      this.classActive = false
+      document.body.style.overflowY = "scroll";
+    }
+    else {
+      this.classActive = true;
+      document.body.style.overflowY = "hidden";
+    }
+
+
+  }
+
+  /* $(".navT").on("click", function(){
+     $(this).toggleClass("active");
+     $("#menu").toggleClass("open");
+     $(".content").toggleClass("shift");
+     
+     
+   })*/
 
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
+
 
   constructor() { }
 
