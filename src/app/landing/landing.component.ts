61a382052b75a822e4287be563b6c36bd663a3f5
Jan Steindorf <funstyle@hotmail.de>
2022-02-07 04:27:51 +0100

    Created Animations + new AOS Library + responsive changes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,3 +1,4 @@
+import { isDelegatedFactoryMetadata } from '@angular/compiler/src/render3/r3_factory';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -9,10 +10,84 @@ import { RouterModule } from '@angular/router';
 
 export class LandingComponent implements OnInit {
 
+  whatText: number = 1;
 
-  constructor() { }
+  newTextPos: number = 0;
 
-  ngOnInit(): void {
+  isDeleting: boolean = true;
+  isTimeout: boolean = false;
+
+  myArray = [
+    "Frontend Developer",
+    "Game Programmer",
+    "Unity Programmer",
+    "Designer",
+    "Not the woman",
+    "Gamer",
+    "Friend",
+    "Dreamer",
+    "Your Choice :)"
+  ]
+  id;
+  constructor() {
+
   }
 
+  ngOnInit(): void {
+    this.changeText()
+  }
+
+  changeText(): void {
+    setTimeout(() => {
+      this.isDeleting = true;
+      this.isTimeout = false;
+      this.id = setInterval(() => {
+        this.DeleteText();
+        this.AddText();
+      }, 100);
+    }, 2000);
+
+
+
+
+  }
+
+  DeleteText(): void {
+    if (this.isDeleting == true) {
+      let text = document.getElementById('myJobs');
+      let length = text.innerHTML.length;
+
+
+      if (length == 0)
+        this.isDeleting = false;
+
+
+      text.innerHTML = text.innerHTML.slice(0, -1);
+    }
+
+  }
+
+  AddText(): void {
+    if (this.isDeleting == false && this.isTimeout == false) {
+
+      let text = document.getElementById('myJobs');
+
+      this.newTextPos = text.innerHTML.length;
+
+      text.innerHTML += this.myArray[this.whatText].charAt(this.newTextPos);
+
+      if (text.innerHTML.length == this.myArray[this.whatText].length) {
+        this.isTimeout = true;
+        setTimeout(() => {
+          this.isDeleting = true;
+          this.isTimeout = false;
+        }, 1000);
+        if (this.whatText == (this.myArray.length - 1))
+          this.whatText = 0
+        else
+          this.whatText++;
+      }
+    }
+  }
 }
+
